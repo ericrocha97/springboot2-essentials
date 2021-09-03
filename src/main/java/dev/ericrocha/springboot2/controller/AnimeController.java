670b2f7f9042c06010af3c512eee7e71873d165e
Eric Rocha <ericrochaeric@gmail.com>
2021-09-03 13:56:40 -0300

    aula 08

--- src/main/java/dev/ericrocha/springboot2/controller/AnimeController.java
+++ src/main/java/dev/ericrocha/springboot2/controller/AnimeController.java
@@ -3,10 +3,12 @@ package dev.ericrocha.springboot2.controller;
 import dev.ericrocha.springboot2.domain.Anime;
 import dev.ericrocha.springboot2.service.AnimeService;
 import dev.ericrocha.springboot2.util.DateUtil;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +25,13 @@ public class AnimeController {
 
 
     @GetMapping()
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return animeService.ListAll() ;
+        return ResponseEntity.ok(animeService.ListAll());
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        return ResponseEntity.ok(animeService.findById(id));
     }
 }
--- src/main/java/dev/ericrocha/springboot2/service/AnimeService.java
+++ src/main/java/dev/ericrocha/springboot2/service/AnimeService.java
@@ -1,14 +1,24 @@
 package dev.ericrocha.springboot2.service;
 
 import dev.ericrocha.springboot2.domain.Anime;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class AnimeService {
+    private List<Anime> animes = List.of(new Anime(1L,"DBZ"), new Anime(2L,"Death Note"));
     //private final AnimeRepository animeRepository
     public List<Anime> ListAll(){
-        return List.of(new Anime(1L,"DBZ"), new Anime(2L,"Death Note"));
+        return animes;
+    }
+
+    public Anime findById(long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not found"));
     }
 }
