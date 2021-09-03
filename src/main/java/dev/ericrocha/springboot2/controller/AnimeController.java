b17a5766ed5d4a5be4f4d24e2d8af87167217699
Eric Rocha <ericrochaeric@gmail.com>
2021-09-03 11:51:48 -0300

    Initial commit

---
+++ src/main/java/dev/ericrocha/springboot2/controller/AnimeController.java
@@ -0,0 +1,27 @@
+package dev.ericrocha.springboot2.controller;
+
+import dev.ericrocha.springboot2.domain.Anime;
+import dev.ericrocha.springboot2.util.DateUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("anime")
+@Log4j2
+@AllArgsConstructor
+public class AnimeController {
+    private DateUtil dateUtil;
+
+
+    @GetMapping(path = "list")
+    public List<Anime> list(){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return List.of(new Anime("DBZ"), new Anime("Death Note"));
+    }
+}
